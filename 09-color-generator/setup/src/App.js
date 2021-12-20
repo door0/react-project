31527f8fff7e2847086c5692dd36082e1afbe3fb
muey <68139125+door0@users.noreply.github.com>
2021-12-20 17:32:43 +0900

    mutiple inputs ex) color generator - color values and error

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -10,7 +10,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello');
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch(error) {
+      setError(true)
+      console.log(error);
+    }
+    
   }
 
   return (
@@ -18,7 +25,7 @@ function App() {
       <section className="container">
         <h4>color generator</h4>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder="#f15025"/>
+          <input type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder="#f15025" className={`${error?'error':null}`}/>
           <button className="btn" type="submit">submit</button>
         </form>
       </section>
