42e838a24eabe60025f750955042e65ada5e8705
muey <68139125+door0@users.noreply.github.com>
2021-12-20 15:15:22 +0900

    mutiple inputs ex) lorem-ipsum  - intro

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,31 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello world');
+  }
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className="section-center">
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className="lorem-form" onSubmit={handleSubmit}>
+      <label htmlFor="amount" >
+        paragraphs:
+      </label>
+      <input type="number" name="amount" id="amount" value={count}
+      onChange={(e)=> setCount(e.target.value)} />
+      <button type="submit" className="btn">generate</button>
+    </form>
+    <article className="lorem-text">
+      <p>elly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.</p>
+      <p>elly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.</p>
+    </article>
+  </section>
     )
 }
 
