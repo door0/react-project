4563881bae6b7ffc30cd48fa2b29837dd043a1bf
muey <68139125+door0@users.noreply.github.com>
2021-12-22 17:45:13 +0900

    Color Generator - Single Color

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -12,7 +12,8 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
+      setList(colors);
+      //console.log(colors);
     } catch(error) {
       setError(true)
       console.log(error);
@@ -30,7 +31,12 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index)=> {
+          console.log(color);
+          return (
+            <SingleColor key={index} {...color} index={index} />
+          );
+        })}
       </section>
     </>
   );
